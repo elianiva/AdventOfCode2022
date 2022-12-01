18035b48ff6a61c9b5d6344a6784684ff37f8b75
elianiva <dicha.arkana03@gmail.com>
2022-12-01 13:46:32 +0700

    chore: init day1

---
+++ Solutions/Day1/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2022.Solutions.Day1;
+
+internal sealed class Solution
+{
+    public static int FirstPart(string path)
+    {
+        List<int> calories = new();
+        IEnumerable<string> lines = File.ReadLines(path);
+
+        int i = 0;
+        foreach (ReadOnlySpan<char> line in lines)
+        {
+            if (line == "\n")
+            {
+                i++;
+                continue;
+            }
+
+            _ = int.TryParse(line, out int calory);
+            calories[i] += calory;
+        }
+
+        return calories.Max();
+    }
+}
---
+++ Tests/Day1/Day1Test.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode2022.Solutions.Day1;
+using Xunit;
+
+namespace Tests.Day1;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldBeAbleToSolveExampleInput()
+    {
+        int maxCalory = Solution.FirstPart("./ExampleInput");
+        Assert.Equal(24_000, maxCalory);
+    }
+}
