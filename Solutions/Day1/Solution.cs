58e5fe80dbd1f317292051798d458045560c2e8a
Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>
2022-12-01 16:22:00 +0700

    finished day 1

--- Solutions/Day1/Solution.cs
+++ Solutions/Day1/Solution.cs
@@ -1,29 +1,43 @@
-﻿using System;
+﻿using System.IO;
+using System.Linq;
 using System.Collections.Generic;
-using System.IO;
+using System.Text;
+using System;
 
 namespace AdventOfCode2022.Solutions.Day1;
 
 internal sealed class Solution
 {
-    public static int FirstPart(string path)
+    private static IEnumerable<int> GetCalories(Stream fileStream)
     {
-        List<int> calories = new();
-        IEnumerable<string> lines = File.ReadLines(path);
+        List<int> calories = new() { 0 };
+        using StreamReader reader = new(fileStream, Encoding.UTF8);
 
-        int i = 0;
-        foreach (ReadOnlySpan<char> line in lines)
+        while (!reader.EndOfStream)
         {
-            if (line == "\n")
+            string? line = reader.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
             {
-                i++;
+                calories.Add(0);
                 continue;
             }
 
             _ = int.TryParse(line, out int calory);
-            calories[i] += calory;
+            calories[^1] += calory;
         }
 
+        return calories;
+    }
+
+    public static int FirstPart(Stream fileStream)
+    {
+        IEnumerable<int> calories = GetCalories(fileStream);
         return calories.Max();
+    }
+
+    public static int SecondPart(Stream fileStream)
+    {
+        IEnumerable<int> calories = GetCalories(fileStream);
+        return calories.OrderByDescending(x => x).Take(3).Sum();
     }
 }
--- Tests/Day1/Day1Test.cs
+++ Tests/Day1/Day1Test.cs
@@ -1,14 +1,40 @@
 ﻿using AdventOfCode2022.Solutions.Day1;
+using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Tests.Day1;
 
-public class UnitTest1
+public class Day1Test
 {
+    private readonly ITestOutputHelper output;
+
+    public Day1Test(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
     [Fact]
     public void ShouldBeAbleToSolveExampleInput()
     {
-        int maxCalory = Solution.FirstPart("./ExampleInput");
+        using Stream? exampleInputStream = new FileStream("./Tests/Day1/ExampleInput.txt", FileMode.Open);
+        int maxCalory = Solution.FirstPart(exampleInputStream);
         Assert.Equal(24_000, maxCalory);
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveRealInputFirstPart()
+    {
+        using Stream? inputStream = new FileStream("./Tests/Day1/Input.txt", FileMode.Open);
+        int maxCalory = Solution.FirstPart(inputStream);
+        output.WriteLine($"First Part: {maxCalory}");
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveRealInputSecondPart()
+    {
+        using Stream? inputStream = new FileStream("./Tests/Day1/Input.txt", FileMode.Open);
+        int maxCalory = Solution.SecondPart(inputStream);
+        output.WriteLine($"Second Part: {maxCalory}");
     }
 }
