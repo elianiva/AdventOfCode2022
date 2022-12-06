72da4af9276240753d31d5c57d671cff08f73ed4
Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>
2022-12-06 15:05:33 +0700

    feat: add day 6

---
+++ Solutions/Day6/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022.Solutions.Day6;
+
+internal class Solution
+{
+    public static int Solve(string input, int markerLength)
+    {
+        return input.SkipLast(markerLength)
+                    .TakeWhile((marker, index) => markerLength != input.Skip(index).Take(markerLength).Distinct().Count())
+                    .Count() + markerLength;
+    }
+}
---
+++ Tests/Day6/Day6Test.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode2022.Solutions.Day6;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests.Day6;
+
+public class Day6Test
+{
+    private readonly ITestOutputHelper output;
+
+    public Day6Test(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void ShouldBeAbleToSolveExampleInputFirstPart(string input, int expected)
+    {
+        int count = Solution.Solve(input, 4);
+        Assert.Equal(expected, count);
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveInputFirstPart()
+    {
+        using FileStream? fileStream = File.Open("./Tests/Day6/Input.txt", FileMode.Open);
+        using StreamReader reader = new(fileStream);
+        int count = Solution.Solve(reader.ReadToEnd(), 4);
+        output.WriteLine($"First Part: {count}");
+    }
+
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    public void ShouldBeAbleToSolveExampleInputSecondPart(string input, int expected)
+    {
+        int count = Solution.Solve(input, 14);
+        Assert.Equal(expected, count);
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveInputSecondPart()
+    {
+        using FileStream? fileStream = File.Open("./Tests/Day6/Input.txt", FileMode.Open);
+        using StreamReader reader = new(fileStream);
+        int count = Solution.Solve(reader.ReadToEnd(), 14);
+        output.WriteLine($"First Part: {count}");
+    }
+}
