60d22e4e20ac7819ccd92d597ff0a20c540f5554
Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>
2022-12-07 18:35:50 +0700

    feat: add day 7

---
+++ Solutions/Day7/Solution.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022.Solutions.Day7;
+
+internal record struct TreeFile(int Size, string Name);
+
+internal sealed class TreeNode
+{
+    public string Name { get; }
+    public List<TreeFile> Files { get; } = new();
+    public List<TreeNode> Nodes { get; } = new();
+    public TreeNode? ParentNode { get; set; } = null;
+    public int FileSizeCount => Files.Sum(file => file.Size) + Nodes.Sum(node => node.FileSizeCount);
+
+
+    public TreeNode(string nodeName, TreeNode? parentNode = null)
+    {
+        Name = nodeName;
+        ParentNode = parentNode;
+    }
+
+    public TreeNode GetNode(string nodeName) => Nodes.Where(node => node.Name == nodeName).First();
+}
+
+internal class Solution
+{
+    private const int AVAILABLE_SPACE = 70_000_000;
+    private const int MIN_UNUSED = 30_000_000;
+    private const int MAX_FILE_SIZE = 100_000;
+
+    public static void CollecValidNodes(TreeNode node, List<TreeNode> nodeSizes)
+    {
+        foreach (TreeNode childNode in node.Nodes)
+        {
+            if (childNode.FileSizeCount < MAX_FILE_SIZE)
+            {
+                nodeSizes.Add(childNode);
+            }
+            CollecValidNodes(childNode, nodeSizes);
+        }
+    }
+
+    public static void GetSmallestFreedValue(TreeNode node, List<int> minValues, int usedSpace)
+    {
+        int nodeSize = node.FileSizeCount;
+        int currentSize = usedSpace - nodeSize;
+        if (AVAILABLE_SPACE - currentSize >= MIN_UNUSED)
+        {
+            minValues.Add(nodeSize);
+        }
+
+        foreach (TreeNode childNode in node.Nodes)
+        {
+            GetSmallestFreedValue(childNode, minValues, usedSpace);
+        }
+    }
+
+    private static TreeNode BuildTree(IEnumerable<string> lines)
+    {
+        TreeNode root = new("/");
+
+        TreeNode currentNode = root;
+        foreach (string line in lines)
+        {
+            string[] lineSegments = line.Split(' ');
+
+            // cd command
+            if (lineSegments is ["$", "cd", string target])
+            {
+                currentNode = target switch
+                {
+                    "/" => root,
+                    ".." => currentNode.ParentNode!,
+                    string t => currentNode.GetNode(t),
+                    _ => throw new Exception("INVALID"),
+                };
+            }
+
+            // ls command
+            if (lineSegments is ["$", "ls"])
+            {
+                continue;
+            }
+
+            // add dir
+            if (lineSegments is ["dir", string dirName])
+            {
+                currentNode.Nodes.Add(new TreeNode(dirName, currentNode));
+                continue;
+            }
+
+            // add file
+            if (lineSegments is [string fileSizeStr, string fileName])
+            {
+                int fileSize = int.Parse(fileSizeStr);
+                currentNode.Files.Add(new TreeFile(fileSize, fileName));
+            }
+        };
+
+        return root;
+    }
+
+    public static int FirstPart(Stream lines)
+    {
+        TreeNode tree = BuildTree(lines.ToEnumerable());
+        List<TreeNode> validNodes = new();
+        CollecValidNodes(tree, validNodes);
+        return validNodes.Sum(node => node.FileSizeCount);
+    }
+
+    public static int SecondPart(Stream lines)
+    {
+        TreeNode tree = BuildTree(lines.ToEnumerable());
+        int usedSpace = tree.FileSizeCount;
+        List<int> minValues = new();
+        GetSmallestFreedValue(tree, minValues, usedSpace);
+        return minValues.Min();
+    }
+}
---
+++ Tests/Day7/Day7Test.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode2022.Solutions.Day7;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests.Day7;
+
+public class Day7Test
+{
+    private readonly ITestOutputHelper output;
+
+    public Day7Test(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveExampleInputFirstPart()
+    {
+        using Stream? exampleInputStream = File.Open("./Tests/Day7/ExampleInput.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+        int totalSize = Solution.FirstPart(exampleInputStream);
+        Assert.Equal(95437, totalSize);
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveExampleInputSecondPart()
+    {
+        using Stream? exampleInputStream = File.Open("./Tests/Day7/ExampleInput.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+        int totalSize = Solution.SecondPart(exampleInputStream);
+        Assert.Equal(24933642, totalSize);
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveInputFirstPart()
+    {
+        using Stream? exampleInputStream = File.Open("./Tests/Day7/Input.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+        int totalSize = Solution.FirstPart(exampleInputStream);
+        output.WriteLine($"First Part: {totalSize}");
+    }
+
+    [Fact]
+    public void ShouldBeAbleToSolveInputSecondPart()
+    {
+        using Stream? exampleInputStream = File.Open("./Tests/Day7/Input.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+        int totalSize = Solution.SecondPart(exampleInputStream);
+        output.WriteLine($"Second Part: {totalSize}");
+    }
+}
